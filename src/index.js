f80717457cebc4508fe2c4813768f63c5ace3172
Oscar Estrella <estrella71oeec@gmail.com>
2023-09-01 17:28:48 -0300

    port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from './app';
 import './database';
+import { PORT } from './config'
 
-app.listen(3000)
-console.log('server on port', 3000); 
+app.listen(PORT)
+console.log('server on port', PORT); 
